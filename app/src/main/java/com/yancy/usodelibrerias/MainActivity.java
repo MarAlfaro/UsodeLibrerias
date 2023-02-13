2dd31d4d214612647607b3a4575f1204b9eabdf0
MarAlfaro <alfaromargarita556@gmail.com>
2023-02-12 23:54:54 -0600

    Update

--- app/src/main/java/com/yancy/usodelibrerias/MainActivity.java
+++ app/src/main/java/com/yancy/usodelibrerias/MainActivity.java
@@ -2,13 +2,46 @@ package com.yancy.usodelibrerias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainActivity extends AppCompatActivity {
-
+    SliderView sliderView;
+    Button btnSitioWeb;
+    private String url;
+    int[] imagenes = {R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(imagenes);
+
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+        btnSitioWeb = findViewById(R.id.btnSitioWeb);
+        btnSitioWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://doramasflix.co/capitulos/goblin-1x1"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/yancy/usodelibrerias/SliderAdapter.java
@@ -0,0 +1,51 @@
+package com.yancy.usodelibrerias;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+public class SliderAdapter extends SliderViewAdapter<SliderAdapter.Holder>{
+
+    int[] imagenes;
+
+    public SliderAdapter(int[] images){
+
+        this.imagenes = images;
+
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.slider_item,parent,false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder viewHolder, int position) {
+
+        viewHolder.imageView.setImageResource(imagenes[position]);
+
+    }
+
+    @Override
+    public int getCount() {
+        return imagenes.length;
+    }
+
+    public class Holder extends  SliderViewAdapter.ViewHolder{
+
+        ImageView imageView;
+
+        public Holder(View itemView){
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_view);
+
+        }
+    }
+
+}
